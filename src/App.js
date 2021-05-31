878167d824c65f6882e260a5371d7b4fbf5bde52
Jordan Masone <jordanmasone1@gmail.com>
2021-05-31 11:16:44 -0400

    Added fade in function.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from "react";
-import Typist from 'react-typist';
+import React, { useState } from "react";
+import Header from "./components/Header";
 import "./style.css";
 
 function App() {
+
+  const [aniDone, setAniDone] = useState(false);
+
   return (
     <div className="App">
-      <Typist cursor={{ show: false }}>
-        <h1>Hello, my name is <u>Jordan Masone.</u></h1>
-        <h3>I am a Full Stack Developer with a focus on the MERN stack.</h3>
-        <h3>Make sure to check out my projects page to see my latest projects.</h3>
-        <h3>Contact me for inquiries at: <a href="mailto:jordanmasone1@gmail.com?subject = Full Stack Developer Inquiry">jordanmasone1@gmail.com</a></h3>
-      </Typist>
+      <Header setAniDone={setAniDone} />
     </div >
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Typist from 'react-typist';
+
+export default function Header({ setAniDone }) {
+
+    const fadeIn = () => {
+        setInterval(show, 100);
+    };
+
+    const show = () => {
+        const target = document.getElementById("fade");
+        let opacity = Number(window.getComputedStyle(target).getPropertyValue("opacity"));
+
+        if (opacity < 1) {
+            opacity += 0.1;
+            target.style.opacity = opacity
+        } else {
+            clearInterval(0);
+            setAniDone(true);
+        }
+    }
+
+    return (
+        <>
+            <Typist cursor={{ show: false }} onTypingDone={() => fadeIn()}>
+                <h1>Hello, my name is Jordan Masone</h1>
+                <Typist.Delay ms={500} />
+                <h1>
+                    I am a <u>Front End Developer</u>
+                    <Typist.Backspace count={20} delay={500} />
+                    <Typist.Delay ms={500} /> <u>Back End Developer</u>
+                    <Typist.Backspace count={19} delay={500} />
+                    <Typist.Delay ms={500} /> <a href="https://github.com/jmasone15">Full Stack Developer</a>
+                </h1>
+            </Typist>
+            <div id="fade" className="fade">
+                <h3>Check out my projects page to see my latest projects.</h3>
+                <h3>Contact me for inquiries at: <a target="_blank" rel="noreferrer" href="mailto:jordanmasone1@gmail.com?subject = Full Stack Developer Inquiry">jordanmasone1@gmail.com</a></h3>
+            </div>
+        </>
+    )
+}
