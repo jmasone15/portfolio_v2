1207a629250a7ed3c04ee872d038823dbf6b02c7
Jordan Masone <jordanmasone1@gmail.com>
2021-05-31 16:10:46 -0400

    Added hover cards for project info.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -14,6 +14,70 @@ export default function Projects() {
             <div className="p-body">
                 <h5>blah blah blah blah blah blah blah blah blah blah blah blah</h5>
             </div>
+            <div className="p-container">
+                <div className="container">
+                    <div className="p-card">
+                        <div className="face face1">
+                            <div className="content">
+                                <div className="icon">
+                                    <i className="fa fa-linkedin-square" style={{ backgroundColor: "#e07768" }} aria-hidden="true">
+                                        <h2 style={{ backgroundColor: "#e07768" }}>Project Name</h2>
+                                    </i>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="face face2">
+                            <div className="content" style={{ backgroundColor: "#dee2ff" }}>
+                                <p style={{ backgroundColor: "#dee2ff" }}>blah blah blah blah blah blah blah blah blah blah</p>
+                                <br />
+                                <button>Project buttons</button>
+                                <button>Project buttons</button>
+                                <button>Project buttons</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="p-card">
+                        <div className="face face1">
+                            <div className="content">
+                                <div className="icon">
+                                    <i className="fa fa-linkedin-square" style={{ backgroundColor: "#6eadd4" }} aria-hidden="true">
+                                        <h2 style={{ backgroundColor: "#6eadd4" }}>Project Name</h2>
+                                    </i>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="face face2">
+                            <div className="content" style={{ backgroundColor: "#dee2ff" }}>
+                                <p style={{ backgroundColor: "#dee2ff" }}>blah blah blah blah blah blah blah blah blah blah</p>
+                                <br />
+                                <button>Project buttons</button>
+                                <button>Project buttons</button>
+                                <button>Project buttons</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="p-card">
+                        <div className="face face1">
+                            <div className="content">
+                                <div className="icon">
+                                    <i className="fa fa-linkedin-square" style={{ backgroundColor: "#4aada9" }} aria-hidden="true">
+                                        <h2 style={{ backgroundColor: "#4aada9" }}>Project Name</h2>
+                                    </i>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="face face2">
+                            <div className="content" style={{ backgroundColor: "#dee2ff" }}>
+                                <p style={{ backgroundColor: "#dee2ff" }}>blah blah blah blah blah blah blah blah blah blah</p>
+                                <br />
+                                <button>Project buttons</button>
+                                <button>Project buttons</button>
+                                <button>Project buttons</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div >
             <Footer />
         </>
     )
---
+++ src/utils/projectData.js
@@ -0,0 +1,68 @@
+const projects = [
+    {
+        id: "jO2rJ8%TD*0@fFwTC8dx9k*S7rD_Ra",
+        name: "¿Donde?",
+        img: "https://via.placeholder.com/175",
+        description: "This application was designed for the everyday person. Ask yourself this question; Have I ever needed a vacation so badly, but did not know where to start...yeah of course you have, we ALL have. Welcome to Dondé, (where) we take the hard work of planning Trips out of your hands! Our site helps optimize your travel itinerary to let you make the most out of your trip! This application features a full React front end, MongoDB backend, and full user authentication.",
+        github: "https://github.com/jmasone15/travel-planner-project",
+        deploy: "https://shielded-woodland-30004.herokuapp.com/"
+    },
+    {
+        id: "$^6_rD^E%5bl0vSp^KRbaXdPj*q77B",
+        name: "Build-a-Date",
+        img: "https://via.placeholder.com/175",
+        description: "Many people have a hard time deciding what they would like to do with their significant others for dates! This project was centered around an application that allows you to build a date that is specific to your taste and preferences, and saves that data so that you can have access to it whenever you want! It takes away the difficult parts of planning! Enjoy building your date!",
+        github: "https://github.com/jmasone15/build-a-date",
+        deploy: "https://vast-gorge-65423.herokuapp.com/"
+    },
+    {
+        id: "3XMqzOtg9jELmTQsP9nD3%GEmJd!!S",
+        name: "Leftover / Hangover",
+        img: "https://via.placeholder.com/175",
+        description: "Do you ever have lots of food in your kitchen, but no idea what to cook? How about lots of alcohol, but no idea what drink to make? Not anymore, thanks to our application, Leftover Hangover. Leftover Hangover is a fridge to table application that allows the user to input the ingredients they have in their kitchens, and returns some possible recipes they could make. On the other side of the application, users can input what alcohol or mixers they have, and the website will return some drink recipes they could make. Every recipe on our website is accompanied by a picture, prep time, caloric information, and link to the recipe isntructions. With Leftover Hangover, you never have to worry about not knowing what to make again!",
+        github: "https://github.com/JRLaro/JJAM",
+        deploy: "https://jrlaro.github.io/JJAM/"
+    },
+    {
+        id: "IYEVpIrACb#o*NL-3voZICZ@EstkcI",
+        name: "Google Books Search",
+        img: "https://via.placeholder.com/175",
+        description: "The Google Books Search Application is a program that allows the user to search for books through the Google Books API. This app features a React front end, MongoDB backend, and a full user authentication system. Once the user searches for a book, they can save that book to their own profile and keep a record of their saved books.",
+        github: "https://github.com/jmasone15/google-books-react-homework",
+        deploy: "https://guarded-brook-67536.herokuapp.com/"
+    },
+    {
+        id: "izzGMORefzi&L4G%0XYB6iEeGoU5q=",
+        name: "Employee Directory",
+        img: "https://via.placeholder.com/175",
+        description: "This project allows the user to view all of their employees information rapidly and effectively. With the use of React Components, the user can search for a certain number of randomly generated employees and see each employees basic information. The user can then select any particular employee to see more sensitive data about that particular employee.The random generated employees come from the Random User API which is a free, open- source API that anyone can use. The user can also sort their search by name, or filer their search by gender, if they choose to do so. This application features a full React front end.",
+        github: "https://github.com/jmasone15/employee-directory-homework",
+        deploy: "https://jmasone15.github.io/employee-directory-homework/"
+    },
+    {
+        id: "McYV6CcgQDXu-$YUdWiQVyx$_D#$IW",
+        name: "README Generator",
+        img: "https://via.placeholder.com/175",
+        description: "This application is one of my favorites, the README generator. After a long, tiring week of working on a project, the last thing you want to do is worry about the README. That's why this README generator application is handy for any web developer. Through answering questions with the inquirer package, you can build out your custom resume in no time!",
+        github: "https://github.com/jmasone15/readme-generator-homework",
+        deploy: "https://jmasone15.github.io/readme-generator-homework/"
+    },
+    {
+        id: "Z*^AYt0MFP0t*yaWvJv6Qws#CY3e0V",
+        name: "Password Generator",
+        img: "../images/PassGen1.jpg",
+        description: "The generator itself can make a randomly generated password between 8 and 128 characters. The user has the choice to edit their generated password as they please with a few options. The generator will ask the user if they want to include lower case characters, upper case characters, numbers, and special characters in their password. Taking into account the requested length of the password, the program will then generate a random passcode with all of thsoe specifications.",
+        github: "https://github.com/jmasone15/password_Generator_Homework",
+        deploy: "https://jmasone15.github.io/password_Generator_Homework/"
+    },
+    {
+        id: "F$tN0Kslitkt!Tu17Sg_yrs%HOzfqt",
+        name: "Budget Tracker",
+        img: "https://via.placeholder.com/175",
+        description: "This application helps the user keep track of their spending throughout their daily life. Featuring a beautiful chart design, the user can clearly see and edit their budget, expenses, and revenue. Making this application convenient for the everyday consumer.",
+        github: "https://github.com/jmasone15/budget_tracker_homework",
+        deploy: "https://blooming-reef-62861.herokuapp.com/"
+    }
+]
+
+module.exports = projects;
