4c39bdf72528b038398ed3b0568fc2ef0e3904b9
Jordan Masone <jordanmasone1@gmail.com>
2021-06-02 15:36:48 -0400

    Heroku error

--- server.js
+++ server.js
@@ -17,9 +17,6 @@ app.use(morgan("tiny")); // logging framework
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    });
 }
 
 // Database Connection
@@ -36,3 +33,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/portoflioBacken
 
 // Routes
 app.use("/auth", require("./routes/userRoutes"));
+
+// Heroku
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
